3d05ad18413455dc143b908f314a51fc1515d626
ND4 <diakonovnikolay@gmail.com>
2025-05-16 05:44:41 +0300

    Update numberOfRecentCalls.cpp

--- tasks/easy/numberOfRecentCalls.cpp
+++ tasks/easy/numberOfRecentCalls.cpp
@@ -23,3 +23,22 @@ public:
  * RecentCounter* obj = new RecentCounter();
  * int param_1 = obj->ping(t);
  */
+
+
+/// ------------------------------------------- Хорошее решение:
+class RecentCounter {
+private:
+    std::vector<int> records;
+    int start;
+
+public:
+    RecentCounter() : start(0) {}
+
+    int ping(int t) {
+        records.push_back(t);
+        while (records[start] < t - 3000) {
+            start++;
+        }
+        return records.size() - start;
+    }
+};
